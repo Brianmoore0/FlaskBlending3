8d4af84616e00b5ff0417f2feb9e8cf1e2afdec5
Brian Moore <brianmoore@Brians-MacBook-Pro.local>
2020-01-03 21:19:50 -0600

    Initial commit

---
+++ AutoBlendSolverV4.py
@@ -0,0 +1,54 @@
+from sympy.solvers import solve
+from sympy import Symbol
+
+x = Symbol('x')
+
+btrvp = int(58)
+targetvl = int(105)
+targetrvp = float(14.9)
+
+
+class AutoBlendInfo:
+    def __init__(self, location, tankvol, actualrvp, actualvl, actualt50):
+        self.location = location
+        self.tankvol = tankvol
+        self.actualrvp = actualrvp
+        self.actualvl = actualvl
+        self.actualt50 = actualt50
+
+    def auto_calc_vl(self):
+        solution = (abs(round(((float(self.actualvl) - float(targetvl)) / 2) / 100 * int(self.tankvol), 1)))
+        return solution
+
+    def auto_calc_rvp(self):
+        if float(self.actualrvp) < targetrvp:
+            sol = (solve(
+                ((float(targetrvp) * (int(self.tankvol) + x)) - (int(self.tankvol) * float(self.actualrvp))) / x
+                - int(btrvp)
+                , x, dict=True))
+            return (round((sol[0][x]), 1))
+        else:
+            return "WARNING: Blend not possible, actual RVP is above target"
+
+    def auto_calc_t50(self):
+        sol = round((float(self.actualt50) - 182) / 1.5 / 100 * (int(self.tankvol)), 1)
+        if float(self.actualt50) >182:
+            return(sol)
+        else:
+            return None
+
+    def show_auto_calc(self):
+        print("\n")
+        print("Based on RVP: " + self.location + " needs " + str(self.auto_calc_rvp()) + " BBLs of butane")
+        print("Based on V/L: " + self.location + " needs " + str(self.auto_calc_vl()) + " BBLs of butane")
+        print("Based on T50: " + self.location + " needs " + str(self.auto_calc_t50()) + " BBLs of butane")
+        print("\n")
+
+    def final_auto_calc(self):
+        finalval = round((min(float(self.auto_calc_vl()), float(self.auto_calc_rvp()), float(self.auto_calc_t50()))), 2)
+        return finalval
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from AutoBlendSolverV4 import AutoBlendInfo
+from flask_material import Material
+
+
+app = Flask(__name__)
+Material(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blend.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Blend(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(200), nullable=False)
+    tankvolume = db.Column(db.String(200), default=0)
+    actualrvp = db.Column(db.String(200), nullable=False)
+    actualvl = db.Column(db.String(200), nullable=False)
+    actualt50 = db.Column(db.String(200))
+    finalcalc = db.Column(db.String(200))
+
+
+    def __repr__(self):
+        return '<Blend %r>' % self.id
+
+
+@app.route('/')
+def index():
+    blends = Blend.query.order_by(Blend.id).all()
+    return render_template("index.html", blends=blends)
+
+@app.route('/newblend/', methods=['POST', 'GET'])
+def NewBlend():
+    if request.method == 'POST':
+        newlocation = request.form['newlocation']
+        newtankvolume = request.form['newtankvolume']
+        newactualrvp = request.form['newactualrvp']
+        newactualvl = request.form['newactualvl']
+        newactualt50 = request.form['newactualt50']
+        new_blend = Blend(location=newlocation, tankvolume=newtankvolume,actualrvp=newactualrvp, actualvl=newactualvl, actualt50=newactualt50)
+
+        try:
+            db.session.add(new_blend)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'the blend didnt save correctly'
+    else:
+        return render_template("newblend.html")
+
+
+@app.route('/autocalc/<int:id>')
+def Calculate(id):
+    blend = Blend.query.get_or_404(id)
+    newcalc = AutoBlendInfo(blend.location,blend.tankvolume,blend.actualrvp, blend.actualvl, blend.actualt50)
+    blend_bbls = newcalc.final_auto_calc()
+    blend.finalcalc = blend_bbls
+
+    try:
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'The blend did not calculate correctly'
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    blend_to_delete = Blend.query.get_or_404(id)
+
+    try:
+        db.session.delete(blend_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem updating the blend'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    blend = Blend.query.get_or_404(id)
+
+    if request.method =='POST':
+        blend.location = request.form['location']
+        blend.tankvolume = request.form['tankvolume']
+        blend.actualrvp = request.form['actualrvp']
+        blend.actualvl = request.form['actualvl']
+        blend.actualt50 = request.form['actualt50']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your blend'
+
+    else:
+        return render_template('update.html', blend=blend)
+
+
+if __name__ == '__main__':
+    app.run()
